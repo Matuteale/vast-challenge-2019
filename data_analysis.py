00c11a116628a80b4798f58a4119ddfca88ceca4
matuteale <mbuscagl@itba.edu.ar>
2019-06-20 15:18:12 -0300

    Add user_count_by_location_grouped_by_hour

--- data_analysis.py
+++ data_analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import csv
 
 def count_user_tweets(data):
   grouped = data.groupby('account')
@@ -14,16 +15,32 @@ def users_that_tweet_at_diff_locations(data):
   filtered = grouped_unique_locations[grouped_unique_locations > 1]
   print(filtered)
 
+def user_count_by_location_grouped_by_hour(data, writeCSV):
+  times = pd.DatetimeIndex(data.time)
+  grouped = data.groupby([times.month, times.day, times.hour, 'location'])['account'].nunique()
+  print(grouped)
+  if writeCSV:
+    path = './output/user_count_by_location_grouped_by_hour.csv'
+    outcsv = open(path, 'w')
+    outcsv.truncate()
+    writer = csv.writer(outcsv)
+    writer.writerow(['month', 'day', 'hour', 'location', 'unique_accounts_count'])
+    outcsv.close()
+    grouped.to_csv(path, mode = 'a', header = False)
+
 def main():
-  print("\n\nReading csv data...\n\n")
-  data = pd.read_csv("./data/MC3/Yint.csv")
-  print("\n\nCounting tweets by user...\n\n")
-  count_user_tweets(data)
-  print("\n\nCounting retweets by user...\n\n")
-  count_user_retweets(data)
-  print("\n\nCalculating users that tweet at different locations...\n\n")
-  users_that_tweet_at_diff_locations(data)
+  print('\n\nReading csv data...\n\n')
+  data = pd.read_csv('./data/MC3/Yint.csv')
+  data.info()
+  user_count_by_location_grouped_by_hour(data, True)
+  # print('\n\nCounting tweets by user...\n\n')
+  # count_user_tweets(data)
+  # print('\n\nCounting retweets by user...\n\n')
+  # count_user_retweets(data)
+  # print('\n\nCalculating users that tweet at different locations...\n\n')
+  # users_that_tweet_at_diff_locations(data)
 
 
-if __name__== "__main__":
+
+if __name__== '__main__':
   main()
