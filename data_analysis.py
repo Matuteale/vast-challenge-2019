5516b3b7129919e208df88f3fe270467b6322d2c
matuteale <mbuscagl@itba.edu.ar>
2019-06-23 18:22:45 -0300

    Add influencers function

--- data_analysis.py
+++ data_analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import csv
 from fuzzywuzzy import fuzz
+from collections import OrderedDict
 
 sewer_and_water = ["discharged", "discharge", "drain", "drainage", "flood", "hygiene", "irrigation", "pipes", "pump", "river", "sanitary", "sewage", "sewer", "stream", "underground", "wash", "waste", "water"]
 power = ["valve", "heat", "gas", "power", "electric", "candle", "flashlight", "generator", "black out", "blackout", "dark", "radiation", "radio rays", "energy", "nuclear", "fuel", "battery", "radiant"]
@@ -8,6 +9,14 @@ roads_and_bridges = ["airport", "avenue", "bridge", "bus", "congestion", "drive"
 medical = ["medical", "red cross", "food", "emergency", "urgent", "evacuate", "evacuating", "evacuation", "protection", "ambulance", "escape", "first aid", "rescue", "rescuing", "dead", "death", "kill", "help", "help out", "help with", "volunteer", "volunteering", "explosion", "exploding", "explode", "victim", "fatalities"]
 buildings = ["collapse", "housing", "house"]
 full_retweets_keywords = ['re:']
+
+def writeCSVFromData(data, path, header):
+  outcsv = open(path, 'w')
+  outcsv.truncate()
+  writer = csv.writer(outcsv)
+  writer.writerow(header)
+  outcsv.close()
+  data.to_csv(path, mode = 'a', header = False, index_label=False, index=False)
 
 def count_user_tweets(data):
   grouped = data.groupby('account')
@@ -34,12 +43,11 @@ def findKeywordsInMessageAndAppendToData(data, keywords, message, row, keyword_c
   return data
 
 def keyword_count_by_location_grouped_by_hour_retweets_only(data, writeCSV):
+  data = data.loc[data['message'].str.find('re:') == 0]
   data['keyword_category'] = ''
   new_data = pd.DataFrame({'time':[], 'location': [], 'account': [], 'message': [], 'keyword_category': []})
   for i, row in data.iterrows():
-    if type(row['message']) is not str or row['message'].find('re:') == 0:
-      new_data = findKeywordsInMessageAndAppendToData(new_data, full_retweets_keywords, row['message'], row, 'retweets')
-      continue
+    new_data = findKeywordsInMessageAndAppendToData(new_data, full_retweets_keywords, row['message'], row, 'retweets')
     if i % 500 == 0:
       print('row: ' + str(i))
 
@@ -47,22 +55,14 @@ def keyword_count_by_location_grouped_by_hour_retweets_only(data, writeCSV):
   new_data.index = pd.to_datetime(new_data['time'])
   grouped = new_data.groupby([pd.Grouper(freq='10Min'), 'location', 'keyword_category'])['keyword_category'].count()
   print(grouped)
-
   if writeCSV:
-    path = './output/keyword_count_by_location_grouped_by_hour_retweets_only.csv'
-    outcsv = open(path, 'w')
-    outcsv.truncate()
-    writer = csv.writer(outcsv)
-    writer.writerow(['time', 'location', 'keyword_category', 'count'])
-    outcsv.close()
-    grouped.to_csv(path, mode = 'a', header = False)
+    writeCSVFromData(grouped, './output/keyword_count_by_location_grouped_by_hour_retweets_only.csv', ['time', 'location', 'keyword_category', 'count'])
 
 def keyword_count_by_location_grouped_by_hour(data, writeCSV):
+  data = data.loc[data['message'].str.find('re:') != 0]
   data['keyword_category'] = ''
   new_data = pd.DataFrame({'time':[], 'location': [], 'account': [], 'message': [], 'keyword_category': []})
   for i, row in data.iterrows():
-    if type(row['message']) is not str or row['message'].find('re:') == 0:
-      continue
     new_data = findKeywordsInMessageAndAppendToData(new_data, roads_and_bridges, row['message'], row, 'road')
     new_data = findKeywordsInMessageAndAppendToData(new_data, power, row['message'], row, 'power')
     new_data = findKeywordsInMessageAndAppendToData(new_data, medical, row['message'], row, 'rescue')
@@ -76,32 +76,40 @@ def keyword_count_by_location_grouped_by_hour(data, writeCSV):
   print(grouped)
 
   if writeCSV:
-    path = './output/keyword_count_by_location_grouped_by_hour.csv'
-    outcsv = open(path, 'w')
-    outcsv.truncate()
-    writer = csv.writer(outcsv)
-    writer.writerow(['time', 'location', 'keyword_category', 'count'])
-    outcsv.close()
-    grouped.to_csv(path, mode = 'a', header = False)
+    writeCSVFromData(grouped, './output/keyword_count_by_location_grouped_by_hour.csv', ['time', 'location', 'keyword_category', 'count'])
 
 def user_count_by_location_grouped_by_hour(data, writeCSV):
   times = pd.DatetimeIndex(data.time)
   grouped = data.groupby([times.month, times.day, times.hour, 'location'])['account'].nunique()
   print(grouped)
   if writeCSV:
-    path = './output/user_count_by_location_grouped_by_hour.csv'
-    outcsv = open(path, 'w')
-    outcsv.truncate()
-    writer = csv.writer(outcsv)
-    writer.writerow(['month', 'day', 'hour', 'location', 'unique_accounts_count'])
-    outcsv.close()
-    grouped.to_csv(path, mode = 'a', header = False)
+    writeCSVFromData(grouped, './output/user_count_by_location_grouped_by_hour.csv', ['month', 'day', 'hour', 'location', 'unique_accounts_count'])
+
+def influencers_info(data, writeCSV, topNumber):
+  data_without_retweets = data.loc[data['message'].str.find('re:') != 0]
+  grouped_by_account = data_without_retweets.groupby('account')['message'].count()
+  top_account_tweets = grouped_by_account.nlargest(topNumber)
+  finalDataFrame = pd.DataFrame(OrderedDict({'account':[], 'tweets': [], 'mentions': [], 'times_retweeted': []}))
+  data_retweets_only = data.loc[data['message'].str.find('re:') == 0]
+  for account, tweet_count in top_account_tweets.iteritems():
+    mentions = data.loc[(data['account'] != account) & (data['message'].str.find('@' + account) != -1)]
+    tweets = data_without_retweets.loc[data_without_retweets['account'] == account]
+    tweets['message'] = 're: ' + tweets['message'].astype(str)
+    retweets = data_retweets_only.loc[(data_retweets_only['account'] != account) & (data_retweets_only['message'].isin(tweets['message']))]
+    row = pd.DataFrame(OrderedDict({'account':[account], 'tweets': [tweet_count], 'mentions': [mentions.size], 'times_retweeted': [retweets.size]}))
+    finalDataFrame = finalDataFrame.append(row)
+  print(finalDataFrame)
+  if writeCSV:
+    writeCSVFromData(finalDataFrame, './output/influencers_info.csv', ['account', 'tweets', 'mentions', 'times_retweeted'])
 
 def main():
   print('\n\nReading csv data...\n\n')
   data = pd.read_csv('./data/MC3/Yint-no-retweets.csv')
   data.info()
-  keyword_count_by_location_grouped_by_hour(data, True)
+  accounts_to_filter = ['______3333_____', 'Opportunities2', 'Opportunities1']
+  data = data.loc[~data['account'].isin(accounts_to_filter)]
+
+  influencers_info(data, True, 10)
   # print('\n\nCounting tweets by user...\n\n')
   # count_user_tweets(data)
   # print('\n\nCounting retweets by user...\n\n')
